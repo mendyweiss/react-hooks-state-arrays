96417b2bb1e0d7678f679a0c21623f090cd80545
Mendy <mendyw94@gmail.com>
2022-09-24 15:58:43 -0400

    Completed Walkthrough

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,33 +1,70 @@
 import React, { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
 import {nanoid} from "nanoid"
+import { Fragment } from "react/cjs/react.development";
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState('All');
+
+
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    const newFoodArray = [...foodList, newFood];
+    const newFoodArray = [...foods, newFood];
     console.log([...foodList])
     setFoods(newFoodArray);
 
   }
 
   function handleLiClick (id) {
-    const removedFoodsArray = foods.filter((food) => food.id !== id)
-    setFoods(removedFoodsArray)
+    const updatedSpiceLevel = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel : food.heatLevel + 1,
+        }
+      } else {
+          return food;
+        }
+    });
+    setFoods(updatedSpiceLevel)
   }
 
-  const foodList = foods.map((food) => (
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  });
+
+
+  const foodList = foodsToDisplay.map((food) => (
     <li key={nanoid()} id={food.id} onClick={()=> handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
-    console.log(foodList)
+
   return (
-    <div>
-      <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{foodList}</ul>
-    </div>
+    <Fragment>
+      <div>
+        <button onClick={handleAddFood}>Add New Food</button>
+        <ul>{foodList}</ul>
+      </div>
+      <div>
+        <select name="filter" onChange={handleFilterChange}>
+          <option value="All">All</option>
+          <option value="American">American</option>
+          <option value="Sichuan">Sichuan</option>
+          <option value="Thai">Thai</option>
+          <option value="Mexican">Mexican</option>
+        </select>
+      </div>
+    </Fragment>
   );
 }
 
